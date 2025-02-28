907ef6e625eaaa108c6df08d17a41b37cb888ada
estebantapia-encora <esteban.tapia@encora.com>
2025-02-28 14:59:26 -0800

    Added solution for problem 1 in Python, Java and TypeScript

--- src/Java/Problem1.java
+++ src/Java/Problem1.java
@@ -4,17 +4,21 @@ class Solution {
         int i = 0, j = 0;
         while(i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) {
-                finalArray.add(nums1[i++]);
+                finalArray.add(nums1[i]);
+                i++;
             }
             else {
-                finalArray.add(nums2[j++]);
+                finalArray.add(nums2[j]);
+                j++;
             }
     }
     while (i < nums1.length) {
-        finalArray.add(nums1[i++]);
+        finalArray.add(nums1[i]);
+        i++;
     }
     while (j < nums2.length) {
-        finalArray.add(nums2[j++]);
+        finalArray.add(nums2[j]);
+        j++;
     }
     int arrayLength = finalArray.size();
     if (arrayLength % 2 != 0) {
